f288028260502a09fb4c801cab20612427824fe9
Willy Wang <willy.x.wang@gmail.com>
2018-10-11 09:59:10 -0700

    Remove comments.

--- Interation_2/GTPPayrollApp/src/main/java/com/touchpointsinc/payrollapp/WillyTest1.java
+++ Interation_2/GTPPayrollApp/src/main/java/com/touchpointsinc/payrollapp/WillyTest1.java
@@ -6,7 +6,6 @@ import java.util.Calendar;
 public class WillyTest1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Timestamp createTs = new Timestamp(Calendar.getInstance().getTimeInMillis());
 		System.out.println(createTs);
 	}
